b4c25fc24baf5866b1b67289fae1f4289287ebea
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2022-11-21 10:16:01 +0700

    warehouse_full

--- pages/admin/warehouses/index.vue
+++ pages/admin/warehouses/index.vue
@@ -17,8 +17,10 @@
 
     <v-row>
       <v-col cols="12">
-        <v-data-table :headers="headers" :items="warehouses"
-        :items-per-page="-1"
+        <v-data-table
+          :headers="headers"
+          :items="warehouses"
+          :items-per-page="-1"
         >
           <template v-slot:item="row">
             <tr>
@@ -44,8 +46,6 @@
                 >
                   <v-icon small> mdi-square-edit-outline</v-icon>
                 </v-btn>
-
-
               </td>
 
               <td>
@@ -61,82 +61,114 @@
                   <v-icon small> mdi-delete</v-icon>
                 </v-btn>
               </td>
-
-              
             </tr>
           </template>
         </v-data-table>
       </v-col>
     </v-row>
 
-
     <v-overlay
       z-index="0"
       :value="edit_overlay"
       opacity="0.1"
-      style="cursor: pointer;"
-      
+      style="cursor: pointer"
     >
-      
-       <v-card
-       z-index="2" 
-       color="white" height="30vh" width="30vw"
-       class="d-flex align-center"
-       
-       >
-
-       <v-container>
-        <v-row>
-        <v-col
-        cols="1"
-        ></v-col>
-        <v-text-field background-color="grey" outlined ></v-text-field>
-        <v-col
-        cols="1"
-        ></v-col>
-       </v-row>
-
-
-        <v-row 
-        
-        >
-        <v-col 
-        class="d-flex justify-center"
-        cols="6"
-        >
-        
-        <v-btn
-        class="white--text"
-        color="teal"
-        @click="onEditYes()"
+      <v-card
+        v-click-outside="onClickOutside"
+        z-index="2"
+        color="white"
+        height="20rem"
+        width="25rem"
+        class="d-flex align-center"
+        style="cursor: default"
       >
-        Yes
-      </v-btn>
-      
-        </v-col>
+        <v-container>
+          <v-row>
+            <v-col cols="1"></v-col>
+            <h2 class="black--text ml-5 mb-3">ID : {{ this.onOverlay_id }}</h2>
+            <v-col cols="1"></v-col>
+            <h2 class="black--text ml-5 mb-3">
+              Name : {{ this.onOverlay_name }}
+            </h2>
+            <v-col cols="1"></v-col>
+          </v-row>
 
-        <v-col
-        class="d-flex justify-center"
-        cols="6"
-        >
-        
-        <v-btn
-        class="white--text"
-        color="teal"
-        @click="edit_overlay = false"
-      >
-        No
-      </v-btn>
-      
-        </v-col>
+          <v-row class="d-flex justify-center">
+            <v-col cols="10">
+              <v-text-field
+                background-color="grey"
+                outlined
+                v-model="onOverlay_new_name"
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-      </v-row>
-       </v-container>
-       </v-card>
-      
+          <v-row>
+
+            <v-col class="d-flex justify-center" cols="6">
+              <v-btn
+                class="white--text"
+                color="red"
+                @click="edit_overlay = false"
+              >
+                No
+              </v-btn>
+            </v-col>
+
+            <v-col class="d-flex justify-center" cols="6">
+              <v-btn class="white--text" color="teal" @click="onEditYes()">
+                Yes
+              </v-btn>
+            </v-col>
+
+            
+          </v-row>
+        </v-container>
+      </v-card>
     </v-overlay>
 
+    <v-overlay
+      z-index="0"
+      :value="delete_overlay"
+      opacity="0.1"
+      style="cursor: pointer"
+    >
+      <v-card
+        v-click-outside="onClickOutside"
+        z-index="2"
+        color="white"
+        height="20rem"
+        width="25rem"
+        class="d-flex align-center"
+        style="cursor: default"
+      >
 
+     <v-container class="black--text">
+      <v-row class="d-flex justify-center mb-10">
+        <h1>Do you want delete {{this.onOverlay_name}}</h1>
+      </v-row>
+
+      
+        <v-row>
+          <v-col class="d-flex justify-center" cols="6">
+            <v-btn class="white--text" color="red" @click="onDeleteYes()">
+              Yes
+            </v-btn>
+          </v-col>
+
+          <v-col class="d-flex justify-center" cols="6">
+            <v-btn
+              class="white--text"
+              color="teal"
+              @click="delete_overlay = false"
+            >
+              No
+            </v-btn>
+          </v-col>
+        </v-row>
+     </v-container>
+      </v-card>
+    </v-overlay>
   </v-container>
 </template>
   
@@ -148,8 +180,14 @@ export default {
 
   data() {
     return {
-      onOverlay_item:[],
+      onOverlay_item: null,
+      onOverlay_id: "",
+      onOverlay_name: "",
+      onOverlay_new_name: "",
       edit_overlay: false,
+
+      delete_overlay: false,
+
       // zIndex: 0,
       warehouses: [],
       headers: [
@@ -160,15 +198,20 @@ export default {
           value: "warehouse_id",
           // width: "10%",
         },
-        { text: "Name", value: "warehouse_name", 
-        // width: "15%" 
-      },
+        {
+          text: "Name",
+          value: "warehouse_name",
+          // width: "10%"
+        },
+        { text: "Edit", sortable: false },
+        { text: "Delete", sortable: false },
       ],
     };
   },
 
   created() {
     this.getAllWarehouses();
+    this.setEdit();
   },
 
   methods: {
@@ -179,25 +222,78 @@ export default {
       console.log(getAllWarehouses.response);
       this.warehouses = getAllWarehouses.response;
     },
-    onEdit(item) {
-      this.edit_overlay = true;
-      console.log("Edit on " + item.warehouse_id );
-      this.onOverlay_item = item.warehouse_id;
+
+    updateOneWarehouse() {
+      const updateOneWarehouse = this.$axios
+        .$put(
+          "http://localhost:3001/warehouses/edit/" + this.onOverlay_id,
+          this.onOverlay_item
+        )
+        .then((res) => {
+          this.onOverlay_item = new FormData();
+          console.log(res);
+          this.$router.go(0);
+        });
     },
-    onEditYes(){
+
+    deleteOneWarehouse() {
+      const updateOneWarehouse = this.$axios
+        .$delete(
+          `http://localhost:3001/warehouses/delete/${this.onOverlay_id}`,
+          this.onOverlay_item
+        )
+        .then((res) => {
+          this.onOverlay_item = new FormData();
+          console.log(res);
+          this.$router.go(0);
+        });
+    },
+
+    setEdit() {
+      this.onOverlay_item = new FormData();
+    },
+
+    onEdit(item) {
+      this.onOverlay_new_name = "";
+      this.edit_overlay = true;
+      console.log("Edit on " + item.warehouse_id);
+      this.onOverlay_id = item.warehouse_id;
+      this.onOverlay_name = item.warehouse_name;
+    },
+    onEditYes() {
       this.edit_overlay = false;
-      console.log(this.onOverlay_item );
+
+      this.onOverlay_item.append("warehouse_id", this.onOverlay_id);
+      this.onOverlay_item.append("warehouse_name", this.onOverlay_new_name);
+
+      console.log(JSON.stringify(Object.fromEntries(this.onOverlay_item)));
+      this.updateOneWarehouse();
     },
 
     onDelete(item) {
+      this.delete_overlay = true;
       console.log("Delete on " + item.warehouse_id);
+      this.onOverlay_id = item.warehouse_id;
+      this.onOverlay_name = item.warehouse_name;
     },
-    
+
+    onDeleteYes() {
+      this.delete_overlay = false;
+
+      this.onOverlay_item.append("warehouse_id", this.onOverlay_id);
+      this.onOverlay_item.append("warehouse_name", this.onOverlay_name);
+
+      console.log(JSON.stringify(Object.fromEntries(this.onOverlay_item)));
+      this.deleteOneWarehouse();
+    },
+
+    onClickOutside() {
+      this.edit_overlay = false;
+      this.delete_overlay = false;
+    },
   },
 };
 </script>
 
-<style>
 
-</style>
   
